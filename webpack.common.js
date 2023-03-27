0f859e4bd23af5af2e6b1331b7566ed59f45e96c
Zeffry Reynando <zeffry.reynando@gmail.com>
2023-03-27 13:51:28 +0700

    Fix eslint codeceptjs, Fix image optimation, and Fix bundle analyzer default open make deployment to third party stuck

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
 const ImageminMozjpeg = require('imagemin-mozjpeg');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: {
@@ -78,6 +78,9 @@ module.exports = {
         }),
       ],
     }),
-    new BundleAnalyzerPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      openAnalyzer: false,
+    }),
   ],
 };
